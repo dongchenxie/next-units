e7ef50c23fc453d6860ee1dc94fa91a78d993df2
Dongchen Xie <dongchen.xie@visualping.io>
2022-07-23 12:08:44 -0700

    donanana

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,7 @@
 export const AppConfig = {
   site_name: 'DONANANA site starter',
   title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
+  description:
+    'DOnanana Starter code for your Nextjs Boilerplate with Tailwind CSS',
   locale: 'en',
 };
